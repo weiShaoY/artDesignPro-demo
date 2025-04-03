c1bb2b5d95153f978f63ba0d1057bdf4459a748d
weiShaoY_ <1604705673@qq.com>
2025-04-03 18:15:05 +0800

    chore: 🔧暂存

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,11 @@
 import type { App } from 'vue'
+
 import { createPinia } from 'pinia'
+
 export const store = createPinia()
 
 export function initStore(app: App<Element>) {
   app.use(store)
 }
+
+export * from './modules/setting'
